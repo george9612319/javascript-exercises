3b67bb334017668382171c466d359dc24a745e82
george9612319 <130203573+george9612319@users.noreply.github.com>
2023-04-23 02:06:11 -0700

    complete reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    var length = string.length;
+    var reversestring = '';
+    for (var i=length-1; i>=0; i--) {
+        reversestring += string[i];
+    }
+    return reversestring;
 };
 
 // Do not edit below this line
