631a52c3910e04cfafe876964a27bdc43b884dcf
george9612319 <130203573+george9612319@users.noreply.github.com>
2023-04-23 01:27:57 -0700

    complete function of repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string,number) {
+    var repeatString = '';
+    if (number < 0) {
+        return 'ERROR' ;
+    }
 
+    else {
+        for (var i=0; i < number; i++) {
+            repeatString += string;
+            }
+            return repeatString;
+        }
 };
 
 // Do not edit below this line
