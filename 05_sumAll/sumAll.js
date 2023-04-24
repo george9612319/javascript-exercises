449238682e76b717a572ebee44c81263a3f45f32
george9612319 <130203573+george9612319@users.noreply.github.com>
2023-04-23 21:24:02 -0700

    complete the function of sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(int1,int2) {
+    var sum = 0;
+    if (int1 >= 0 && int2 >=0) {
+        if (int1 >= int2) {
+            for (var i=int2; i <= int1; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+        if (int2 > int1) {
+            for (var j=int1; i<=int2; i++) {
+                sum +=i;
+            }
+            return sum;
+        }
+    }
+    else {
+        return "ERROR" ;
+    }
 };
 
 // Do not edit below this line
