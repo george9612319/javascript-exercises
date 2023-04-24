13bec28a1865b019246d50a9d96170f28be51c4d
george9612319 <130203573+george9612319@users.noreply.github.com>
2023-04-23 23:28:26 -0700

    complete two functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    var celsius = (temp*1.8) + 32 ;//convert to celsius
+    parseFloat(celsius.toFixed(1));
+    return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    var Fahrenheit = (temp*9/5) + 32 ;// convert to F 
+    parseFloat(Fahrenheit.toFixed(1));
+    return Fahrenheit;
 };
 
 // Do not edit below this line
