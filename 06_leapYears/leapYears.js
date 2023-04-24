17ba37b0f9e5153cc8b0a4e967579b6c1c45533a
george9612319 <130203573+george9612319@users.noreply.github.com>
2023-04-23 23:04:00 -0700

    complete the function of leapyear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(input) {
+    if ( input % 400 === 0) {
+        return true;
+    }//divid by 400
+    else if (input % 4 === 0 && input % 100 !== 0) {
+        return true;
+    }//divid by 4 but couldn't be divided by 100
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
