a8dbdd2f34da0545611aaa90f173b20d5301b63d
george9612319 <130203573+george9612319@users.noreply.github.com>
2023-04-23 21:32:43 -0700

    All pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(int1,int2) {
     var sum = 0;
-    if (int1 >= 0 && int2 >=0) {
+    if (int1 >= 0 && int2 >=0 && typeof int1 === "number" && typeof int2 === "number") {
         if (int1 >= int2) {
             for (var i=int2; i <= int1; i++) {
                 sum += i;
