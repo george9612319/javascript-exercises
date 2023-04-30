2912522cb4586190ef8e65d455d94022bd14f138
george9612319 <130203573+george9612319@users.noreply.github.com>
2023-04-30 01:06:34 -0700

    complete findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const agearray = [];
+     for (let i=0; i<people.length; i++) {
+        if (people[i].yearOfDeath === undefined){ //call the yearOfBirth and yearOfDeath
+            let age = 2023-people[i].yearOfBirth;
+            agearray.push(age);  //push the age to new array
+        }
+        else {
+        let age = people[i].yearOfDeath - people[i].yearOfBirth;
+        agearray.push(age);  //push the age to new array
+        }
+     }
+    
+     let oldestage = Math.max(...agearray);
+     let index = agearray.indexOf(oldestage);//check the index of the oldest age
+    return people[index]; //return the oldest name
+   
 };
 
 // Do not edit below this line
