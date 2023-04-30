4fc71b201e4fb4216d95245ab183b94cc781fcd4
george9612319 <130203573+george9612319@users.noreply.github.com>
2023-04-29 19:12:21 -0700

    complete palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newstring = string.replace(/[^a-z]/gi,''); //sift out non-alphabet
+    newstring = newstring.toLowerCase();
+    let wordlength = newstring.length // get string length
 
+    //check the word is symmetry by compare the alphabets
+    let index = wordlength-1; 
+    for (var i=0; i<= index/2; i++){
+        if (newstring[i] !== newstring[index-i]) {
+            return false;
+        }
+    } 
+    return true;
 };
 
 // Do not edit below this line
