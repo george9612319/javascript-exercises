0c0ebbc083113d44041024175353b7f39b5878e5
george9612319 <130203573+george9612319@users.noreply.github.com>
2023-04-29 17:44:44 -0700

    complete factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -17,12 +17,20 @@ const multiply = function(array) {
   return array.reduce((total, num) => total *num, 1);}
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum =1;
+  if (num === 0) {
+    return 1;
+  }
+	else {for (var i=num; i>=1; i--) {
+      sum *= i;
+    }
+  return sum;
+  }
 };
 
 // Do not edit below this line
