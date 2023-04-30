dc2b0ed6f28354e17d9915f4c28212bba5e9c20a
george9612319 <130203573+george9612319@users.noreply.github.com>
2023-04-29 23:19:43 -0700

    complete the function of Fibonacci series

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(order) {
+    if (order-1 < 0) { // for order < 0 return oops
+        return 'OOPS'
+    }
+    else if (order === 1 || order === 2) { //for order = 0,1  return 1
+        return 1;
+    }
+    else {
+        const array = [1,1];
+        for (let i=2; i<order; i++) {
+           let sum= array[i-1] + array[i-2];
+            array.push(sum);
+        }
+        return array[order-1];
+    }
 };
 
 // Do not edit below this line
